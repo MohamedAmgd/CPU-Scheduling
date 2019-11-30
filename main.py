3b6099b55d9f7789eb9430c90ab83c8fdcdcc48c
Mohamed Amgd <megapof2@gmail.com>
2019-12-01 01:41:16 +0200

    updated main in 1:40  1/12/2019

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 from Process import process
-from collections import OrderedDict
-
-
-'''
-Iterate over all the key value pairs in dictionary and call the given
-callback function() on each pair. Items for which callback() returns True,
-add them to the new dictionary. In the end return the new dictionary.
-'''
 
 
 def filterTheDict(dic, time):
     newDict = dict()
-    # Iterate over all the items in dictionary and filter items which has even keys
     for (key, value) in dic.items():
-        # Check if key is even then add pair to new dictionary
         if value.getArrival() <= time:
-            newDict[key] = value
-    return newDict
-
-
-def filterFinshedProcesses(dic):
-    newDict = dict()
-    # Iterate over all the items in dictionary and filter items which has even keys
-    for (key, value) in dic.items():
-        # Check if key is even then add pair to new dictionary
-        if value.getRemaining() > 0:
             newDict[key] = value
     return newDict
 
@@ -45,8 +25,8 @@ def avgWaiting(dic):
 
 def printAlgo(dic):
     for i in dic:
-        print("{0}: arrival = {1} brust = {2}  w = {3} t = {4}".format(i, dic[i].getArrival(), dic[i].getBrust(),
-                                                                       dic[i].getWaiting(), dic[i].getTurnaround()))
+        print("{0}: Arrival = {1} Brust = {2}  Waiting = {3} Turn around = {4}".format(i, dic[i].getArrival(), dic[i].getBrust(),
+                                                                                       dic[i].getWaiting(), dic[i].getTurnaround()))
     print("avg Waiting = {0} , avg Turn Around = {1}".format(
         avgWaiting(dic), avgTurnAround(dic)))
 
@@ -71,7 +51,7 @@ def SJF(processes_dic):
         first_key = next(iter(remaing_processes))
         if (remaing_processes[first_key].getArrival() > time):
             time = remaing_processes[first_key].getArrival()
-        waiting_dic = OrderedDict(
+        waiting_dic = dict(
             sorted(filterTheDict(remaing_processes, time).items(), key=lambda x: x[1].getBrust()))
         min_brust = next(iter(waiting_dic))
         processes_dic[min_brust].setWaiting(
@@ -92,7 +72,7 @@ def SRTF(processes_dic):
         first_key = next(iter(remaining_processes))
         if (remaining_processes[first_key].getArrival() > time):
             time = remaining_processes[first_key].getArrival()
-        waiting_dic = OrderedDict(
+        waiting_dic = dict(
             sorted(filterTheDict(remaining_processes, time).items(), key=lambda x: x[1].getRemaining()))
         min_remaining = next(iter(waiting_dic))
         processes_dic[min_remaining].setRemaining(
@@ -112,9 +92,9 @@ def SRTF(processes_dic):
 def RR(processes_dic, q):
     time = processes_dic[next(iter(processes_dic))].getArrival()
     remaining_processes = dict(processes_dic)
-    waiting_dic = OrderedDict()
+    waiting_dic = dict()
     while len(remaining_processes) > 0:
-        waiting_dic = OrderedDict(
+        waiting_dic = dict(
             sorted(filterTheDict(remaining_processes, time).items(), key=lambda x: x[1].getWaitingArrival()))
         if len(waiting_dic) == 0:
             time = remaining_processes[next(
@@ -141,37 +121,6 @@ def RR(processes_dic, q):
             processes_dic[i].getTurnaround() - processes_dic[i].getBrust())
 
     return processes_dic
-
-
-'''
-def RR(processes_dic, q):
-    time = 0
-    remaing_processes = dict(processes_dic)
-    while len(remaing_processes) > 0:
-        remaing_processes = OrderedDict(
-            sorted(remaing_processes.items(), key=lambda x: x[1].getArrival()))
-        current_process_key = next(iter(remaing_processes))
-        remaing_processes[current_process_key].setRemaining(
-            remaing_processes[current_process_key].getRemaining() - q)
-        current_process_value = remaing_processes[current_process_key]
-        remaing_processes.pop(current_process_key)
-        waiting_dic = filterTheDict(remaing_processes, time)
-        for i in waiting_dic:
-            processes_dic[i].setWaiting(processes_dic[i].getWaiting() + q)
-        time += q
-        if current_process_value.getRemaining() > 0:
-            current_process_value.setArrival(time)
-            remaing_processes[current_process_key] = current_process_value
-        else:
-            time += current_process_value.getRemaining()
-            for i in waiting_dic:
-                processes_dic[i].setWaiting(
-                    processes_dic[i].getWaiting() + current_process_value.getRemaining())
-    for i in processes_dic:
-        processes_dic[i].setTurnaround(
-            processes_dic[i].getBrust() + processes_dic[i].getWaiting())
-    return processes_dic
-'''
 
 
 def start():
@@ -233,17 +182,26 @@ def start():
             input_SRTF["P" + str(i+1)] = process(int(arrival), int(brust))
             input_RR["P" + str(i+1)] = process(int(arrival), int(brust))
         if choice == 1:
+            print("================================================")
             print("FCFS :")
             printAlgo(
                 FCFS(dict(sorted(input_FCFS.items(), key=lambda x: x[1].getArrival()))))
+            print("================================================")
+            input("Press any key to continue")
         elif choice == 2:
+            print("================================================")
             print("SJF :")
             printAlgo(
                 SJF(dict(sorted(input_SJF.items(), key=lambda x: x[1].getArrival()))))
+            print("================================================")
+            input("Press any key to continue")
         elif choice == 3:
+            print("================================================")
             print("SRTF :")
             printAlgo(
                 SRTF(dict(sorted(input_SRTF.items(), key=lambda x: x[1].getArrival()))))
+            print("================================================")
+            input("Press any key to continue")
         elif choice == 4:
             q = ""
             while not isinstance(q, int):
@@ -254,9 +212,12 @@ def start():
                         q = ""
                 except:
                     q = ""
+            print("================================================")
             print("RR , Q = {0}:".format(q))
             printAlgo(
                 RR(dict(sorted(input_RR.items(), key=lambda x: x[1].getArrival())), q))
+            print("================================================")
+            input("Press any key to continue")
         elif choice == 5:
             q = ""
             while not isinstance(q, int):
@@ -267,18 +228,24 @@ def start():
                         q = ""
                 except:
                     q = ""
+            print("================================================")
             print("FCFS :")
             printAlgo(
                 FCFS(dict(sorted(input_FCFS.items(), key=lambda x: x[1].getArrival()))))
+            print("================================================")
             print("SJF :")
             printAlgo(
                 SJF(dict(sorted(input_SJF.items(), key=lambda x: x[1].getArrival()))))
+            print("================================================")
             print("SRTF :")
             printAlgo(
                 SRTF(dict(sorted(input_SRTF.items(), key=lambda x: x[1].getArrival()))))
+            print("================================================")
             print("RR , Q = {0}:".format(q))
             printAlgo(
                 RR(dict(sorted(input_RR.items(), key=lambda x: x[1].getArrival())), q))
+            print("================================================")
+            input("Press any key to continue")
 
 
 start()
